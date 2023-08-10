9c3b2ff493c688ab83a08e4cbe8210e306b1abe1
Brenden Olding <oldingb21@gmail.com>
2023-08-09 22:04:48 -0400

    lab complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+const buttonFocus = () => {
+    console.log('Good!')
+}
+
+const buttonBlur = () => {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe () {
+    return (
+        <>
+            <h1>I am Eyes on Me</h1>
+            <button onFocus={buttonFocus} onBlur={buttonBlur}>Eyes on me</button>
+        </>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
+const passwordChange = e => {
+    console.log("Entering password...")
+}
 
 function Keypad (){
     return (
-        <div></div>
+        <>
+            <h1>I am Keypad</h1>
+            <input type='password' onChange={passwordChange}></input>
+        </>
     )
 }
 
